d37e9180657560ee41cea0bca3be6dea6a9d7adf
Narashimabalaji <116134721+Narashimabalaji@users.noreply.github.com>
2025-06-23 23:12:01 +0700

    Update admin.py

--- routes/admin.py
+++ routes/admin.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, request, jsonify
 from models.users import approve_job,reject_job
-admin_bp = Blueprint("admin", __name__)
+admin_blp = Blueprint("admin", __name__)
 
-@admin_bp.route("/approve-job/<job_id>", methods=["POST"])
+@admin_blp.route("/approve-job/<job_id>", methods=["POST"])
 def approve(job_id):
     approve_job(job_id)
     return jsonify({"message": "Job approved"}), 200
 
-@admin_bp.route("/reject_job/<job_id>", methods=["POST"])
+@admin_blp.route("/reject_job/<job_id>", methods=["POST"])
 def reject(job_id):
     reject_job(job_id)
     return jsonify({"message": "rejected"}), 200
