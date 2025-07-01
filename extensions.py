d25142388380245e3c6a994a7b6bdd87356550c9
Narashimabalaji <116134721+Narashimabalaji@users.noreply.github.com>
2025-07-02 00:33:12 +0700

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,4 +1,9 @@
-# extensions.py
-from flask_sqlalchemy import SQLAlchemy
+# # extensions.py
+# from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
+
+from pymongo import MongoClient
+client = MongoClient("mongodb://localhost:27017")  # or your Render URL
+db = client["job_portal"]  # or your DB name
+
