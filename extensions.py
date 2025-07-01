6282300a698feacf32712963e269d76583a2219f
Narashimabalaji <116134721+Narashimabalaji@users.noreply.github.com>
2025-07-02 00:33:54 +0700

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -4,6 +4,6 @@
 # db = SQLAlchemy()
 
 from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017")  # or your Render URL
+client = MongoClient("mongodb+srv://vijayprabakaran1905:Mongodbhirehub@cluster0.uma8of4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # or your Render URL
 db = client["job_portal"]  # or your DB name
 
